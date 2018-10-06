d41f8e550ab4888f6990944176501380c896388d
Cedric van Putten <me@bycedric.com>
2018-10-06 20:11:42 +0200

    feature: add flow type of reports

---
+++ custom-types/commitlint-format.js
@@ -0,0 +1,22 @@
+// @flow
+declare module '@commitlint/format' {
+	declare type RuleIssue = {
+		valid: boolean,
+		level: number,
+		name: string,
+		message: string,
+	};
+
+	declare type ReportResult = {
+		valid: boolean,
+		input: string,
+		errors: RuleIssue[],
+	};
+
+	declare type Report = {
+		valid: boolean,
+		errorCount: number,
+		warningCount: number,
+		results: ReportResult[],
+	};
+}
